2b8feed277cc87b4cd2359f0f9912cb6779415cb
Muhammad Taaha <96954628+TSTRIVER@users.noreply.github.com>
2023-03-01 13:46:26 +0530

    Update footer.js

--- frontend/src/component/layout/footer/footer.js
+++ frontend/src/component/layout/footer/footer.js
@@ -16,14 +16,14 @@ const Footer = () => {
         <h1>Vintage Lucknowee</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; VintageLucknowee</p>
       </div>
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="/">Instagram</a>
+        <a href="https://www.youtube.com/@sayequahussain3738">Youtube</a>
+        <a href="https://www.facebook.com/sayequa.hussain.1">Facebook</a>
       </div>
     </footer>
   );
